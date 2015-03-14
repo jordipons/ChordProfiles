8b8d8c573700caedb11d631d964642b877e3c83e
idrojsnop <idrojsnop@gmail.com>
2015-03-14 15:14:45 +0100

    Xavi updated chords

--- example.m
+++ example.m
@@ -7,13 +7,15 @@ clc;clear all;close all;
 %
 %       C     C#    D     D#    E     F     F#    G     G#    A     A#    B
 
-%chord=[0     0     1     0     1     0     0     0     1     0     0     1 ]; % E - NOT WORKING!
+%chord=[0     0     1     0     1     0     0     0     1     0     0     1 ]; % E - NOT WORKING with correlation!
 %chord=[1     0     0     0     1     0     0     1     0     0     0     1 ]; % CM7
 %chord=[10    1     1     1     12    1     0     11    2     1     0     11]; % CM7 with noise!
- chord=[1     0     1     0     1     0     0     1     0     0     1     0 ]; % C9 
+%chord=[1     0     1     0     1     0     0     1     0     0     1     0 ]; % C9 
 %chord=[0     0     0     0     1     0     0     0     1     0     0     1 ]; % EM
 %chord=[0     0     1     0     1     0     1     1     0     0     0     1 ]; % Em7(9)
 %chord=[0     0     1     0     0     0     1     0     0     1     0     0 ]; % DM
 
 %get the chord name:
-chord_name = findChordName(chord,'correlation')
+chord_name = findChordName(chord,'binary')
+
+%chord_name = findChordName(chord,'correlation')
